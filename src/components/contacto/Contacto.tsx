a370f5b69f4cfb385a55fd36d7ed3f673629245e
Gaston Avigliano <gastonavigliano@gmail.com>
2021-03-14 20:57:46 -0300

    Finished Contacto style

--- src/components/contacto/Contacto.tsx
+++ src/components/contacto/Contacto.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import styles from './Contacto.module.css'
+
 import DoubleBox from '../double-box/DoubleBox'
 import InformacionDeContacto from './InformacionDeContacto'
 import Formulario from './Formulario'
 
 export default function Contacto() {
   return (
-    <div>
+    <div className={styles.container}>
       <DoubleBox
         leftPane={<InformacionDeContacto />}
         rightPane={<Formulario />}
