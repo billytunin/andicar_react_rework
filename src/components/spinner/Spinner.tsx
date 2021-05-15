9a4ba11bcb824f41becb6c303bf86dd65facb0cd
Gaston Avigliano <gastonavigliano@gmail.com>
2021-05-15 00:48:32 -0300

    change andicar spinner

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -11,10 +11,10 @@ const Spinner = () => {
       viewBox="0 0 100 100"
       preserveAspectRatio="xMidYMid"
     >
-      <circle cx="50" cy="50" r="32" strokeWidth="8" stroke="#e15b64" strokeDasharray="50.26548245743669 50.26548245743669" fill="none" strokeLinecap="round" transform="rotate(287.558 50 50)">
+      <circle cx="50" cy="50" r="32" strokeWidth="8" stroke="#2196f3" strokeDasharray="50.26548245743669 50.26548245743669" fill="none" strokeLinecap="round" transform="rotate(287.558 50 50)">
         <animateTransform attributeName="transform" type="rotate" dur="1s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
       </circle>
-      <circle cx="50" cy="50" r="23" strokeWidth="8" stroke="#f8b26a" strokeDasharray="36.12831551628262 36.12831551628262" strokeDashoffset="36.12831551628262" fill="none" strokeLinecap="round" transform="rotate(-287.558 50 50)">
+      <circle cx="50" cy="50" r="23" strokeWidth="8" stroke="#e4339cbf" strokeDasharray="36.12831551628262 36.12831551628262" strokeDashoffset="36.12831551628262" fill="none" strokeLinecap="round" transform="rotate(-287.558 50 50)">
         <animateTransform attributeName="transform" type="rotate" dur="1s" repeatCount="indefinite" keyTimes="0;1" values="0 50 50;-360 50 50"></animateTransform>
       </circle>
     </svg>
