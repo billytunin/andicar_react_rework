24d09e93d0b6b7e7340db9295257a55ff3db413a
Gaston Avigliano <gastonavigliano@gmail.com>
2021-05-25 22:24:36 -0300

    Change localhost back to catslab real URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/andicar_dev' /* DELETE ME: revert this */
+export const BASE_URL = 'https://catslab.herokuapp.com/andicar_dev'
 export const CDNEdgeUrl = 'http://d2t6xcnqt5h66z.cloudfront.net/juguetes/'
 export const PHOTOS_URL = 'http://photos.andicar.com.ar/'
 export const DEFAULT_PAGINADO = 12
